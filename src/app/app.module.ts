59f7eb1fe405c03eeaa1f001a10ac84a280db263
ThengaraiMaharajan <tmaharajan1999@gmail.com>
2022-07-05 17:42:03 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OfferRideComponent } from './offer-ride/offer-ride.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OfferRideComponent
   ],
   imports: [
     BrowserModule,
